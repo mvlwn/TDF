4071089a696e901fcbd706f2fc18a39a508697fe
Martijn van Leeuwen <m.vanleeuwen@websend.nl>
2012-06-28 17:22:21 +0200

    Added mailer hosts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do
     redirect_to summary_path, :notice => "Deze pagina is niet toegankelijk"
   end
+
+  before_filter :set_mailer_host
 
   helper_method :riders_sort_column, :riders_sort_direction
 
@@ -24,6 +27,10 @@ class ApplicationController < ActionController::Base
 
   def riders_sort_order
     riders_sort_column + " " + riders_sort_direction
+  end
+
+  def set_mailer_host
+    ActionMailer::Base.default_url_options = {:host => request.host_with_port}
   end
 
 

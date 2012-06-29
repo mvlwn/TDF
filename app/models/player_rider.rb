1c4f26d6f9a9446f36b208a03b1aabb8f4f3dd3f
Martijn van Leeuwen <m.vanleeuwen@websend.nl>
2012-06-29 22:32:30 +0200

    PlayerRider gefixt

--- app/models/player_rider.rb
+++ app/models/player_rider.rb
@@ -7,11 +7,18 @@ class PlayerRider < ActiveRecord::Base
   validates :rider_id, :player_id, :presence => true
   validates :rider_id, :uniqueness => {:scope => :player_id}
   validates :player_id, :uniqueness => {:scope => :rider_id}
+  validate :enough_budget
 
   after_create :count_points
 
   def count_points
     player.update_attribute(:points, player.riders.sum(:points))
+  end
+
+  def enough_budget
+    if rider.price > player.budget
+      errors.add(:rider_id, "is te duur")
+    end
   end
 
 

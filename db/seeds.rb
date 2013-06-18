5c3124c27f4a0ee19d9ed3e1002bb58886a51f72
Martijn van Leeuwen <mail@martijnvanleeuwen.nl>
2013-06-18 21:29:00 +0200

    Juiste ritten toegevoegd

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,27 @@ RiderTextParser.new(File.new(riders_file, 'r').read).parse
 
 # Create Stages
 Stage.create([
-  {:number => 0,  :name => "Proloog Liège > Liège",                       :ridden_on => "2012-06-30", :description => "Proloog",         :distance => "6.4"},
-  {:number => 1,  :name => "Liège > Seraing",                             :ridden_on => "2012-07-01", :description => "Vlak",            :distance => "198"},
-  {:number => 2,  :name => "Visé > Tournai",                              :ridden_on => "2012-07-02", :description => "Vlak",            :distance => "207.5"},
-  {:number => 3,  :name => "Orchies > Boulogne-sur-Mer",                  :ridden_on => "2012-07-03", :description => "Middengebergte",  :distance => "197"},
-  {:number => 4,  :name => "Abbeville > Rouen",                           :ridden_on => "2012-07-04", :description => "Vlak",            :distance => "214.5"},
-  {:number => 5,  :name => "Rouen > Saint-Quentin",                       :ridden_on => "2012-07-05", :description => "Vlak",            :distance => "196.5"},
-  {:number => 6,  :name => "Épernay > Metz",                              :ridden_on => "2012-07-06", :description => "Vlak",            :distance => "207.5"},
-  {:number => 7,  :name => "Tomblaine > La Planche des Belles Filles",    :ridden_on => "2012-07-07", :description => "Middengebergte",  :distance => "199"},
-  {:number => 8,  :name => "Belfort > Porrentruy",                        :ridden_on => "2012-07-08", :description => "Middengebergte",  :distance => "157.5"},
-  {:number => 9,  :name => "Arc-et-Senans > Besançon",                    :ridden_on => "2012-07-09", :description => "Tijdrit",         :distance => "41.5"},
-  {:number => 10, :name => "Mâcon > Bellegarde-sur-Valserine",            :ridden_on => "2012-07-11", :description => "Hooggebergte",    :distance => "194.5"},
-  {:number => 11, :name => "Albertville > La Toussuire - Les Sybelles",   :ridden_on => "2012-07-12", :description => "Hooggebergte",    :distance => "148"},
-  {:number => 12, :name => "Saint-Jean-de-Maurienne > Annonay Davézieux", :ridden_on => "2012-07-13", :description => "Middengebergte",  :distance => "226"},
-  {:number => 13, :name => "Saint-Paul-Trois-Châteaux > Le Cap d’Agde",   :ridden_on => "2012-07-14", :description => "Vlak",            :distance => "217"},
-  {:number => 14, :name => "Limoux > Foix",                               :ridden_on => "2012-07-15", :description => "Hooggebergte",    :distance => "191"},
-  {:number => 15, :name => "Samatan > Pau",                               :ridden_on => "2012-07-16", :description => "Vlak",            :distance => "158.5"},
-  {:number => 16, :name => "Pau > Bagnères-de-Luchon",                    :ridden_on => "2012-07-18", :description => "Hooggebergte",    :distance => "197"},
-  {:number => 17, :name => "Bagnères-de-Luchon > Peyragudes",             :ridden_on => "2012-07-19", :description => "Hooggebergte",    :distance => "143.5"},
-  {:number => 18, :name => "Blagnac > Brive-la-Gaillarde",                :ridden_on => "2012-07-20", :description => "Vlak",            :distance => "222.5"},
-  {:number => 19, :name => "Bonneval > Chartres",                         :ridden_on => "2012-07-21", :description => "Tijdrit",         :distance => "53.5"},
-  {:number => 20, :name => "Rambouillet > Paris Champs-Élysées",          :ridden_on => "2012-07-22", :description => "Vlak",            :distance => "120"}
+  {:number => 1,  :name => "Porto-Vecchio - Bastia", :ridden_on => "2013-06-29", :description => "Vlak", :distance => "212"},
+  {:number => 2,  :name => "Bastia - Ajaccio", :ridden_on => "2012-06-30", :description => "Heuvel", :distance => "154"},
+  {:number => 3,  :name => "Ajaccio - Calvi", :ridden_on => "2012-07-01", :description => "Heuvel", :distance => "145"},
+  {:number => 4,  :name => "Nice - Nice", :ridden_on => "2012-07-02", :description => "Ploegentijdrit", :distance => "25"},
+  {:number => 5,  :name => "Cagnes-sur-Mer - Marseille", :ridden_on => "2012-07-03", :description => "Vlak", :distance => "219"},
+  {:number => 6,  :name => "Aix-en-Provence - Montpellier", :ridden_on => "2012-07-04", :description => "Vlak", :distance => "176"},
+  {:number => 7,  :name => "Montpellier - Albi", :ridden_on => "2012-07-05", :description => "Vlak", :distance => "205"},
+  {:number => 8,  :name => "Castres - Ax-3 Domaines", :ridden_on => "2012-07-06", :description => "Berg", :distance => "194"},
+  {:number => 9,  :name => "Saint-Girons - Bagnères-de-Bigorre", :ridden_on => "2012-07-07", :description => "Berg", :distance => "165"},
+  {:number => 10,  :name => "Saint-Gildas-des-Bois - Saint-Malo", :ridden_on => "2012-07-09", :description => "Vlak", :distance => "193"},
+  {:number => 11, :name => "Avranches - Mont Saint-Michel", :ridden_on => "2012-07-10", :description => "Tijdrit", :distance => "33"},
+  {:number => 12, :name => "SaintFougères - Tours", :ridden_on => "2012-07-11", :description => "Vlak", :distance => "218"},
+  {:number => 13, :name => "Tours - Saint-Amand-Montrond", :ridden_on => "2012-07-12", :description => "Vlak", :distance => "173"},
+  {:number => 14, :name => "Saint-Pourçain-sur-Sioule - Lyon", :ridden_on => "2012-07-13", :description => "Vlak", :distance => "191"},
+  {:number => 15, :name => "Givors - Mont Ventoux", :ridden_on => "2012-07-14", :description => "Hooggebergte", :distance => "242"},
+  {:number => 16, :name => "Vaison-la-Romaine - Gap", :ridden_on => "2012-07-16", :description => "Heuvel", :distance => "168"},
+  {:number => 17, :name => "Embrun - Chorges", :ridden_on => "2012-07-17", :description => "Tijdrit", :distance => "32"},
+  {:number => 18, :name => "Gap - Alpe d'Huez", :ridden_on => "2012-07-18", :description => "Berg", :distance => "168"},
+  {:number => 19, :name => "Bourg d'Oisans - Le Grand Bornand", :ridden_on => "2012-07-19", :description => "Berg", :distance => "204"},
+  {:number => 20, :name => "Annecy - Annecy Semnoz", :ridden_on => "2012-07-20", :description => "Berg", :distance => "125"},
+  {:number => 21, :name => "Versailles - Parijs", :ridden_on => "2012-07-21", :description => "Vlak", :distance => "118"}
 ])
 
 player = Player.create({
